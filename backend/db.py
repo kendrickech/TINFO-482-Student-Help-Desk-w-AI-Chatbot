e619605b7c6c1fbe9f42b7f96eaf33e3e3dad005
Kendrick Echevarria <echevken@uw.edu>
2026-02-11 11:46:13 -0800

    shared database should work

--- backend/db.py
+++ backend/db.py
@@ -1,28 +1,17 @@
 # db.py
 import os
-from dotenv import load_dotenv
 import psycopg2
+from dotenv import load_dotenv
 from psycopg2.extras import RealDictCursor
 from pathlib import Path
 
 env_path = Path(__file__).resolve().parent / ".env"
-load_dotenv(dotenv_path=env_path)
+loaded = load_dotenv(dotenv_path=env_path, override=True)
+
 
 def get_db_connection():
-    """
-    Returns a new PostgreSQL connection.
-    RealDictCursor makes query results behave like dictionaries:
-    row["email"] instead of row[0]
-    """
-    """return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        port=int(os.getenv("DB_PORT", 5432)),
-        dbname=os.getenv("DB_NAME", "helpdesk"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", "password"),
-        cursor_factory=RealDictCursor,
-    )"""
     
+
     try:
         connection = psycopg2.connect(
             user=os.getenv("DB_USER"),
